4dc17f1d9ab088bc46fc7f92db630a448af5fa34
Anastasia Minchukova <anastasiaminchukova72@gmail.com>
2022-06-23 12:54:15 +0300

    'lab4'

---
+++ lab4/Receiver.cpp
@@ -0,0 +1,64 @@
+﻿#include<windows.h>
+#include <iostream>
+#include <fstream>
+#include <codecvt>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "File name:\n";
+    string file;
+    cin >> file;
+    ofstream out(file, ios::binary | ios::out);
+    out.close();
+    ifstream in(file, ios::binary | ios::in);
+    cout << "Records maximum number:\n";
+    int record;
+    cin >> record;
+    cout << "Processes number:\n";
+    int process;
+    cin >> process;
+
+    HANDLE semProcess = CreateSemaphore(NULL, -process, 0, L"ready");
+    HANDLE semRec = CreateSemaphore(NULL, record, record, L"isFilled");
+
+    STARTUPINFO* si = new STARTUPINFO[process];
+    PROCESS_INFORMATION* pi = new PROCESS_INFORMATION[process];
+    ZeroMemory(si, process * sizeof(si[0]));
+    for (int i = 0; i < process; i++) {
+        si[i].cb = sizeof(STARTUPINFO);
+        CreateProcess(NULL, (LPWSTR)wstring_convert<codecvt_utf8<wchar_t>>().from_bytes("Sender.exe " + file + " " + to_string(process)).c_str(), NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &(si[i]), &(pi[i]));
+    }
+
+    WaitForSingleObject(semProcess, INFINITE);
+
+    HANDLE hEvent = CreateEvent(NULL, true, false, L"message");
+
+    while (true) {
+        cout << "1 - read a message\n2 - exit\n";
+        int answer;
+        cin >> answer;
+        if (answer == 1) {
+            unsigned strLen;
+            in.read((char*)&strLen, sizeof(unsigned));
+            if (in.eof()) {
+                cout << "There are no messages.\n";
+                in.clear();
+                in.close();
+                return 0;
+            }
+            else {
+                string message;
+                message.resize(strLen);
+                in.read((char*)message.data(), strLen);
+                ReleaseSemaphore(semRec, 1, NULL);
+                cout << message << '\n';
+            }
+        }
+        else {
+            in.close();
+            return 0;
+        }
+    }
+}
---
+++ lab4/Sender/Sender.cpp
@@ -0,0 +1,45 @@
+﻿#include <windows.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    HANDLE hSemProcess = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"ready");
+    HANDLE hSemRecord = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"isFilled");
+    HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"message");
+    ReleaseSemaphore(hSemProcess, 1, NULL);
+
+    while (true) {
+        cout << "1 - send a message\n2 - exit\n";
+        int answer;
+        cin >> answer;
+        if (answer == 1) {
+            if (WaitForSingleObject(hSemRecord, 0) != WAIT_OBJECT_0) {
+                cout << "there is no space for a message. Wait...\n";
+                WaitForSingleObject(hSemRecord, INFINITE);
+            }
+
+            cout << "message:\n";
+            string message;
+            string str;
+            do {
+                cin >> str;
+                if (str.size() > 19) {
+                    cout << "string length must be less than 20\n";
+                }
+            } while (str.size() > 19);
+
+            message = str;
+            unsigned strLen = message.size();
+            ofstream out(argv[1], ios::binary | ios::out | ios::app);
+            out.write((char*)&strLen, sizeof(unsigned));
+            out.write((char*)message.c_str(), message.size());
+            out.close();
+            PulseEvent(hEvent);
+        }
+        else {
+            return 0;
+        }
+    }
+}
